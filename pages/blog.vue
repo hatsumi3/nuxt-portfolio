49c3f30018af99fc134366fac7124198d8e368e7
hatsumi3 <al.usamiku39@gmail.com>
2023-06-26 11:19:07 +0900

    feat: check nuxtpage.

---
+++ pages/blog.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Blog page.</h1>
+    <p class="text">hello.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+
+.text {
+  color: green;
+}
+</style>
